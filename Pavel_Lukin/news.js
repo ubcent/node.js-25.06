e510a358497b72b74b752e5373de4ffce3aea3c5
Pavel <pavel.lukin@web.de>
2019-07-15 18:11:20 +0200

    Домашнее задание для 3 урока, исправления

--- Pavel_Lukin/news.js
+++ Pavel_Lukin/news.js
@@ -5,6 +5,7 @@ request('https://lenta.ru', function(error, response, html) {
     if(!error && response.statusCode == 200) {
         let $ = cheerio.load(html);
         let cols = $('.b-yellow-box__wrap').find('div');
+		
         for(let i = 1; i < cols.length; i++) {
             console.log('%s', $(cols[i]).text());
             console.log('');
--- Pavel_Lukin/translator.js
+++ Pavel_Lukin/translator.js
@@ -22,4 +22,5 @@ async function translat() {
         translat();
     }
 }
+
 translat();
