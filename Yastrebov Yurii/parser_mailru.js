483de36642ee87f550b6c9725906065a6614f8c8
netproblemmm <ubhawk1@mail.ru>
2019-07-15 20:59:25 +0300

    Закончена разработка парсера сайта новостей

--- Yastrebov Yurii/parser_mailru.js
+++ Yastrebov Yurii/parser_mailru.js
@@ -3,6 +3,8 @@ const cheerio = require ('cheerio');
 request('https://yandex.ru/', (err, response, html) => {
   if (!err && response.statusCode === 200) {
     const $ = cheerio.load(html);
-    console.log($('.news__item-content').eq(0).text().trim());
+    for (let i = 0; i < 5; i++) {
+      console.log($('.news__item-content').eq(i).text().trim());
+    }
   }
 });
