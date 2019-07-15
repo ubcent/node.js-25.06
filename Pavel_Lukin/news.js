c989c9d1d9ec3dba2f89215c4de5d555004d0c5c
Pavel <pavel.lukin@web.de>
2019-07-15 18:09:21 +0200

    k

--- Pavel_Lukin/news.js
+++ Pavel_Lukin/news.js
@@ -2,9 +2,9 @@ const request = require('request');
 const cheerio = require('cheerio');
 
 request('https://lenta.ru', function(error, response, html) {
-    if (!error && response.statusCode == 200) {
-        var $ = cheerio.load(html);
-        var cols = $('.b-yellow-box__wrap').find('div');
+    if(!error && response.statusCode == 200) {
+        let $ = cheerio.load(html);
+        let cols = $('.b-yellow-box__wrap').find('div');
         for(let i = 1; i < cols.length; i++) {
             console.log('%s', $(cols[i]).text());
             console.log('');
