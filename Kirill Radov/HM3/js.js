349093d96212eb2c470da05a6093841be59a3368
kirillradov <kirill-radov@mail.ru>
2019-07-16 17:10:44 +0300

    добавил локальный сервер на ноде
    Попробовал чеализовать работу с локальным сервером и яндекс апи.. С кодировкой пока не могу разобраться как сделать правильно, что бы не менял пробелы на + и другие знаки препинания %код

--- Kirill Radov/HM3/js.js
+++ Kirill Radov/HM3/js.js
@@ -3,21 +3,18 @@ new Vue({
     data() {
         return {
             apiKey: 'trnsl.1.1.20190715T222644Z.22d479b4590e609f.cafb43dfb84faf96f18aef07ba49a001ab39b2e1',
+            apiUrl: 'http://localhost:8008',
             text: "",
             data: "",
         }
     },
     methods: {
-
         translate() {
-            axios.get('https://translate.yandex.net/api/v1.5/tr.json/translate', {
-                params: {
-                    key: this.apiKey,
+            axios.get(this.apiUrl,  {
+                    params: {
                     text: this.text,
-                    lang: 'en-ru',
                 }
-            }).then(response => (this.data = response.data.text[0]))
-                .catch(error => (this.info = error))
+            }).then(response => {this.data = response.data[0]}).catch(error => (this.data = error))
         }
     },
 
