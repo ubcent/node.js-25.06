6535aa9a79f79fa29c34d5e8ec0c8d43b276cd45
DMP86 <dmp71086@rambler.ru>
2019-07-22 22:19:47 +0300

    dz4.1

--- Pipekin/index.js
+++ Pipekin/index.js
@@ -19,45 +19,26 @@ app.get('/', (req, res) => {
 app.post('/', (req, res) => {
     const news = []
     let $
-    if( req.body.url === 'Habr') {
-        request('https://habr.com/ru/', function(err, response, body) {
-            if (!err && response.statusCode == 200) {
-                $ = cheerio.load(body)
-                let time, head, prew
-                $('article.post.post_preview').each(function(i, el) {
-                    if(i < req.body.count){
-                        time = $(this).find('span.post__time').text()
-                        head = $(this).find('a.post__title_link').text()
-                        prew = $(this).find('div.js-mediator-article').text()
-                        news.push({time, head, prew})
-                    }    
-                })
-                res.render('index', { 
-                    news,
-                    url: req.body.url,
-                    count: req.body.count
-                } )
-            } else console.log(response.statusCode)
-        })
-           
-    } else if (req.body.url === 'Yandex') {
-        request('https://yandex.ru', function(err, response, body) {
-            if (!err && response.statusCode == 200) {
-                $ = cheerio.load(body)
-
-                $('.news__item-content').each((i, el ) => {
-                    
-                    if(i < req.body.count) news.push({head: $(el).text()})
+    request(req.body.url, function(err, response, body) {
     
-                })
-                res.render('index', { 
-                    news,
-                    url: req.body.url,
-                    count: req.body.count
-                } )       
-            }
-        })    
-    }
+        if (!err && response.statusCode == 200) {
+            $ = cheerio.load(body)
+             let time, head, prew
+            $('article.post.post_preview').each(function(i, el) {
+                if(i < req.body.count){
+                    time = $(this).find('span.post__time').text()
+                    head = $(this).find('a.post__title_link').text()
+                    prew = $(el).find('div.js-mediator-article').text()
+                    news.push({time, head, prew})
+                }    
+            })
+            res.render('index', { 
+                news,
+                url: req.body.url,
+                count: req.body.count
+            } )
+        } else console.log(response.statusCode)
+    })
     
 })
 
