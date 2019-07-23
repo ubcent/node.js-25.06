ff796f502cc8f84e4960a5fa14cb566416aadcbd
Marina Khokkonen <mariskasl@mail.ru>
2019-07-23 13:58:17 +0300

    remove .idea

--- Marina Khokkonen/src/parser.js
+++ Marina Khokkonen/src/parser.js
@@ -12,42 +12,55 @@
 формы дополнительно учитывать передаваемые GET-запросы
 (например, ?count=10&lang=ru и т.д.)*/
 const path = require('path');
-const util = require('util');
+//const util = require('util');
 const request = require('request');
 const cheerio = require('cheerio');
 const consolidate = require('consolidate');
 const express = require('express');
 const bodyParser = require('body-parser');
-const session = require('express-session');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 const urlencodedParser = bodyParser.urlencoded({extended: false});
+
+const tagTypes = [
+                    {tag: "html", selected: false},
+                    {tag: "css", selected: false},
+                    {tag: "javascript", selected: false},
+                    {tag: "react", selected: false},
+                    {tag: "node-js", selected: false},
+                 ];
 
 app.set('views', __dirname + '/public');
 app.engine('hbs', consolidate.handlebars);
 app.set('view engine', 'hbs');
 app.set('views', path.resolve(__dirname, 'views'));
 
-app.use((req, res, next)=>{
-    session({
-        tagNews: 'html',
-    });
-    next();
-});
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+const setTypesArray = ( tag ) => {
+    tagTypes.forEach( el => el.selected = false);
+    tagTypes[tagTypes.findIndex(x => x.tag === tag)].selected = true;
+};
+
 app.get('/', function (req, res)
 {
-    //console.log(req.session.tagNews);
-    res.render('index');
+    res.cookie('tagNews' , ( (typeof req.body.tagNews) !== 'undefined' ? req.body.tagNews : ((typeof req.cookies.tagNews) !== 'undefined' ? req.cookies.tagNews :'html')));
+    res.cookie('countNews' , (req.body.countNews > 0 ? req.body.countNews : ((typeof req.cookies.countNews) !== 'undefined' ? req.cookies.countNews :5)));
+    setTypesArray( req.cookies.tagNews);
+    res.render('index',{countNews: req.cookies.countNews, tagTypes});
 });
 
 app.post('/news', urlencodedParser, function (req, res) {
-    if(!req.body) return res.sendStatus(400);
-    console.log(req.body);
+    if(!req.body) return res.sendStatus(404);
+    res.cookie('tagNews' , req.body.tagNews);
+    res.cookie('countNews' , req.body.countNews);
     let news = [];
     let countNews = 0;
     request(`https://tproger.ru/tag/${req.body.tagNews}/`, (err, response, html) => {
         if (err) {
-            console.log('Не удалось получить страницу: ', err);
+            res.send('Не удалось получить новости: ', err);
         }
         else if (!err && response.statusCode === 200) {
             const $ = cheerio.load(html), posts = $('.post-text');
@@ -61,9 +74,9 @@ app.post('/news', urlencodedParser, function (req, res) {
                 }
                 countNews++;
             });
+            res.render('news', {news:news});
         }
-        else console.log('Ошибка');
-        res.render('news', {news:news});
+        else res.sendStatus(404);
     });
 });
 
