65a912496eaceabc4c4b0dcdf72b2121d817d7d7
dvbondarchuk@gmail.com <dvbondarchuk@gmail.com>
2019-07-30 20:52:57 +0300

    Lesson 5 homework

--- models/task.js
+++ models/task.js
@@ -1,63 +1,10 @@
-const mysql = require('mysql');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const pool = mysql.createPool({
-  host: 'localhost',
-  database: 'todo',
-  user: 'root',
-  password: '',
+const taskSchema = new Schema({
+  title: { type: String },
+  status: { type: String, default: 'uncompleted' },
+  created: { type: Date, default: new Date() },
 });
 
-/* {
-  id: number;
-  title: string;
-  description: string;
-  status: not_started | in_progress | completed;
-  updated: timestamp;
-} */
-module.exports = {
-  getAll() {
-    return new Promise((resolve, reject) => {
-      pool.getConnection((err, connection) => {
-        if(err) {
-          reject();
-        }
-        connection.query('SELECT * from `tasks` WHERE 1', (err, rows) => {
-          if(err) {
-            reject();
-          }
-
-          resolve(rows);
-
-          // возвращаем соединение в пул
-          connection.release();
-        });
-      });
-    });
-  },
-  get(id) {},
-  add(task) {
-    return new Promise((resolve, reject) => {
-      pool.getConnection((err, connection) => {
-        if(err) {
-          reject(err);
-        }
-        connection(
-          'INSERT INTO `tasks` ?',
-          task,
-          (err, result) => {
-            if(err) {
-              reject(err);
-            }
-
-            resolve(result.insertId);
-
-            connection.release();
-          }
-        )
-      });
-    });
-  },
-  update(id, task) {},
-  complete(id) {},
-  delete(id) {},
-}
+module.exports = mongoose.model('Task', taskSchema, 'tasks');
