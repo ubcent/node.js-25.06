5ff8a91d3ecd0f7eda55ffad8289b3fce56b03d5
avgorobec <avgorobec@gmail.com>
2019-08-06 15:09:22 +0300

    Убрал лишие комменты

--- Aleksey Gorobets/routes/todolist/mongo/root.js
+++ Aleksey Gorobets/routes/todolist/mongo/root.js
@@ -4,11 +4,11 @@ const router = express.Router();
 const moment = require('moment');
 const Task = require('./model/taskMongo.js');
 const User = require('./model/user');
-//const passport = require('./passport');
+const passport = require('./passport');
 
-// router.use(passport.initialize());
-// router.use(passport.session());
-// router.use('getalltasks', passport.checkAuthenticated);
+ router.use(passport.initialize());
+ router.use(passport.session());
+ router.use('getalltasks', passport.checkAuthenticated);
 
 router.use(express.static(path.join(__dirname, 'public')));
 
@@ -48,27 +48,27 @@ router.post('/deletetask', async (req, res) => {
     res.redirect('http://localhost:8888/todomongo/getalltasks');
 });
 
-// router.get('/register', async (req, res) => {
-//     if(req.user) {
-//         return res.redirect('/todomongo/getalltasks');
-//     }
-//     res.render('register');
-// });
-//
-// router.post('/register', async (req, res) => {
-//   const user = new User(req.body);
-//   const savedUser = await user.save();
-//   res.redirect('/todomongo/getalltasks');
-// });
-//
-// router.get('/auth', (req, res) => {
-//     if(req.user) {
-//         return res.redirect('/todomongo/getalltasks');
-//     }
-//     res.render('auth', { error: !!req.query.error });
-// });
+ router.get('/register', async (req, res) => {
+     if(req.user) {
+         return res.redirect('/todomongo/getalltasks');
+     }
+     res.render('register');
+ });
 
-//router.post('/auth', passport.authHandler);
+ router.post('/register', async (req, res) => {
+   const user = new User(req.body);
+   const savedUser = await user.save();
+   res.redirect('/todomongo/getalltasks');
+ });
+
+ router.get('/auth', (req, res) => {
+     if(req.user) {
+         return res.redirect('/todomongo/getalltasks');
+     }
+     res.render('auth', { error: !!req.query.error });
+ });
+
+router.post('/auth', passport.authHandler);
 
 router.get('/logout', (req, res) => {
     req.logout();
