4a9615f6d3358b33fa185a4f53d352009d63d299
netproblemmm <ubhawk1@mail.ru>
2019-08-09 21:19:11 +0300

    Закончена разработка TODO листа, но функционал приоритета не до конца работает

--- Yastrebov Yurii/models/task_homework.js
+++ Yastrebov Yurii/models/task_homework.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const taskSchema = new Schema({
   title: {type: String},
-  status: {type: String},
+  status: {type: String, default: "uncompleted"},
+  priority: {type: String, default: "none"},
   created: {type: Date, default: new Date()},
 });
 module.exports = mongoose.model('Task', taskSchema, 'tasks');
